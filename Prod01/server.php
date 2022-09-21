f0605d7ccfa7b0ac8da288f2511af62138766819
Lennen Madere <lm0068@uah.edu>
2022-09-21 18:20:35 -0500

    Post test

--- Prod01/server.php
+++ Prod01/server.php
@@ -1,9 +1,10 @@
 <?php 
     $rectNums = array("1","2","3","4","5","Q");
+    echo htmlspecialchars($_POST["rect1"]);
     foreach ($rectNums as $rectNum) {
         $rectName = "rect" . $rectNum;
         $rectVal = htmlspecialchars($_POST[$rectName]);
-        echo "<p>" . htmlspecialchars($_POST[$rectName]) . "</p>";
+        echo $rectName . "=" . htmlspecialchars($_POST[$rectName]);
         if ($rectVal >= 2) {
             echo "<script>alert(\"Too many students at desk!\")</script>";
         }
