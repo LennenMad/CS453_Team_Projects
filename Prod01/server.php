4ab284184a67d179af1cc5e4fb47d6e61241de5e
Lennen Madere <lm0068@uah.edu>
2022-09-21 12:36:04 -0500

    Changes to how php handles inputs

--- Prod01/server.php
+++ Prod01/server.php
@@ -1,4 +1,12 @@
 <?php 
-    //$formData = $_POST["formdata"];
-    echo "<p>Students at entrance: " . htmlspecialchars($_POST["entranceVal"]) . "</p> ";
+    $studentRects = htmlspecialchars($_POST["rects"]);
+    echo "<p>" . $studentRects . "</p>";
+    foreach ($studentRects as $studentRect) {
+        echo "<p>" . $studentRect . "</p>";
+        /*if ($studentRect <= 2 ) {
+            echo "Too many students in rect!";
+        }*/
+    }
+    echo "<p>Students at entrance: " . htmlspecialchars($_POST["entranceVal"]) . "</p>";
+    
 ?>
