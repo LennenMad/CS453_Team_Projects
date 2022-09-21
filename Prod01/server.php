4ed951e0340d9d6ae79d9aa1ea6842f98c2e43c6
Lennen Madere <lm0068@uah.edu>
2022-09-21 18:06:39 -0500

    Post attempt

--- Prod01/server.php
+++ Prod01/server.php
@@ -2,7 +2,7 @@
     $rectNums = array("1","2","3","4","5","Q");
     foreach ($rectNums as $rectNum) {
         $rectName = "rect" . $rectNum;
-        $rectVal = $_GET[$rectName];
+        $rectVal = htmlspecialchars($_POST[$rectName]);
         if ($rectVal >= 2) {
             echo "<script>alert(\"Too many students at desk!\")</script>";
         }
