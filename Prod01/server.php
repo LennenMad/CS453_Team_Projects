31050e74418c8c8a088edec1ef55d63f315703de
Lennen Madere <lm0068@uah.edu>
2022-09-21 16:59:53 -0500

    testing student numbers

--- Prod01/server.php
+++ Prod01/server.php
@@ -2,9 +2,9 @@
     $studentRects = $_POST["rects"];
     foreach ($studentRects as $studentRect) {
         echo "<p>" . htmlspecialchars($studentRect) . "</p>";
-        /*if ($studentRect <= 2 ) {
-            echo "Too many students in rect!";
-        }*/
+        if ($studentRect >= 2 ) {
+            echo "<script>alert(\"Too many students at desk!\")</script>";
+        }
     }
     //echo "<p>Students at entrance: " . htmlspecialchars($_POST["entranceVal"]) . "</p>";
     
