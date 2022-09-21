d08ea97a679ed7560f686be05d1b71b7906c991a
Lennen Madere <lm0068@uah.edu>
2022-09-21 17:37:56 -0500

    Still trying without buttons

--- Prod01/server.php
+++ Prod01/server.php
@@ -1,8 +1,9 @@
 <?php 
-    $studentRects = $_POST["rects"];
-    foreach ($studentRects as $studentRect) {
-        echo "<p>" . htmlspecialchars($studentRect) . "</p>";
-        if ($studentRect >= 2 ) {
+    $rectNums = array("1","2","3","4","5","Q");
+    foreach ($rectNums as $rectNum) {
+        $rectName = "rect" . $rectNum;
+        $rectVal = $_GET[$rectName];
+        if ($rectVal >= 2) {
             echo "<script>alert(\"Too many students at desk!\")</script>";
         }
     }
