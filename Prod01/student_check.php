67068966e995e7ce7404d5127a332fba64d03b7f
Lennen Madere <lm0068@uah.edu>
2022-09-22 10:20:29 -0500

    Php changes

--- Prod01/student_check.php
+++ Prod01/student_check.php
@@ -1,14 +1,11 @@
 <?php 
-    $rectNums = array("1","2","3","4","5","Q");
-    echo htmlspecialchars($_POST["rect1"]);
-    foreach ($rectNums as $rectNum) {
-        $rectName = "rect" . $rectNum;
-        $rectVal = htmlspecialchars($_POST[$rectName]);
-        echo $rectName . "=" . htmlspecialchars($_POST[$rectName]);
-        if ($rectVal >= 2) {
-            echo "<script>alert(\"Too many students at desk!\")</script>";
-        }
+    echo "Sent val: " . htmlspecialchars($_POST["rect"]);
+
+    $rectVal = htmlspecialchars($_POST["rect"]);
+    if ($rectVal >= 2) {
+        echo "<script>alert(\"Too many students at desk!\")</script>";
     }
+    
     //echo "<p>Students at entrance: " . htmlspecialchars($_POST["entranceVal"]) . "</p>";
     
 ?>
