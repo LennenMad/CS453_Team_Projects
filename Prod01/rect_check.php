f1c553e2cc6262e83a67a96f1f5b8bc096ebd0ec
Lennen Madere <lm0068@uah.edu>
2022-09-22 10:49:26 -0500

    Alert change

--- Prod01/rect_check.php
+++ Prod01/rect_check.php
@@ -1,10 +1,11 @@
 <?php 
-    echo "Sent val: " . htmlspecialchars($_GET["rect"]);
-
     $rectVal = htmlspecialchars($_GET["rect"]);
     if ($rectVal >= 2) {
-        echo "<script>alert(\"Too many students at desk!\")</script>";
+        echo "alert(\"Too many students at desk!\");";
+        exit;
     }
+
+    echo "Sent val: " . htmlspecialchars($_GET["rect"]);
     
     //echo "<p>Students at entrance: " . htmlspecialchars($_POST["entranceVal"]) . "</p>";
     
