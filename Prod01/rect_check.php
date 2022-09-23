afca961af940d80dbdf2b8c82de73bee9ac33581
LennenMadLinux <lenrmadere@gmail.com>
2022-09-23 13:21:52 -0500

    Changes to ajax send, buttons, and check php

--- Prod01/rect_check.php
+++ Prod01/rect_check.php
@@ -1,11 +1,29 @@
 <?php 
-    $rectVal = htmlspecialchars($_GET["rect"]);
+    $rectVal = htmlspecialchars($_GET["value"]);
+    $rectName = htmlspecialchars($_GET["name"]);
     if ($rectVal >= 2) {
-        echo "alert('Too many students at desk!');";
+        if (str_contains($rectName,"rect")) {
+            //Check for which student desk
+            if (str_ends_with($rectName,"1")) {
+                echo "alert('Only one student should be at Dest 1!')";
+            } else if (str_ends_with($rectName,"2")) {
+                echo "alert('Only one student should be at Dest 2!')";
+            } else if (str_ends_with($rectName,"3")) {
+                echo "alert('Only one student should be at Dest 3!')";
+            } else if (str_ends_with($rectName,"4")) {
+                echo "alert('Only one student should be at Dest 4!')";
+            } else if (str_ends_with($rectName,"5")) {
+                echo "alert('Only one student should be at Dest 5!')";
+            } else if (str_ends_with($rectName,"Q")) {
+                echo "alert('Only one student should be in the Question Rectangle!')";
+            }
+        }else {
+            echo "alert('Only the Instructor should be up front!')";
+        }
         exit;
     }
 
-    echo "Sent val: " . htmlspecialchars($_GET["rect"]);
+    echo "" . htmlspecialchars($_GET["value"]);
     
     //echo "<p>Students at entrance: " . htmlspecialchars($_POST["entranceVal"]) . "</p>";
     
