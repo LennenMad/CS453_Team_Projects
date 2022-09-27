f74dff897ec21c5d98dcc3272f6dca3148f07c02
Lennen Madere <lm0068@uah.edu>
2022-09-26 23:44:32 -0500

    no reset

--- Prod01/server.php
+++ Prod01/server.php
@@ -26,23 +26,6 @@
         session_start();
         session_create_id();
         print_r(session_id());
-        $_SESSION['maskFlags'] = array(
-            "1" => 0,
-            "2" => 0,
-            "3" => 0,
-            "4" => 0,
-            "5" => 0,
-            "Q" => 0,
-            "I" => 0,
-        );
-        $_SESSION['lysolFlags'] = array(
-            "1" => 0,
-            "2" => 0,
-            "3" => 0,
-            "4" => 0,
-            "5" => 0,
-            "I" => 0,
-        );
     }
     
     /*
