cb29a6b0e1469d639abe5eeb6305f756b4cd7d74
Matthew Lay <89554334+mdl0016@users.noreply.github.com>
2022-09-26 19:44:46 -0500

    add session generate test

--- Prod01/server.php
+++ Prod01/server.php
@@ -18,6 +18,30 @@
         "I" => 0,
     );
     */
+    function my_session_start() {
+        session_start();
+        // Do not allow to use too old session ID
+        if (!empty($_SESSION['deleted_time']) && $_SESSION['deleted_time'] < time() - 180) {
+            session_destroy();
+            session_start();
+        }
+    }
+
+    function my_session_regenerate_id() {
+        if (session_status() != PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+        $newid = session_create_id('myprefix-');
+        $_SESSION['deleted_time'] = time();
+        session_commit();
+        ini_set('session.use_strict_mode', 0);
+        session_id($newid);
+        session_start();
+    }
+    ini_set('session.use_strict_mode', 1);
+    my_session_start();
+    my_session_regenerate_id();
+
 
     if (!session_id()) {
         echo "No session. Making one... ";
